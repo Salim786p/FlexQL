94d6eb507a5f3e3ea6dfecd65effdaa9bdceb32a
Salim Ansari <ansarisalim786p.com>
2026-04-07 23:18:53 +0530

    Changed the batch size

--- benchmark_flexql.cpp
+++ benchmark_flexql.cpp
@@ -11,7 +11,7 @@ using namespace std;
 using namespace std::chrono;
 
 static const long long DEFAULT_INSERT_ROWS = 10LL; // 100k rows by default for insertion benchmark
-static const int INSERT_BATCH_SIZE = 1;            // if you implement batch inserts in flexql, you can increase this for better performance
+static const int INSERT_BATCH_SIZE = 50000;        // if you implement batch inserts in flexql, you can increase this for better performance
 
 struct QueryStats
 {
